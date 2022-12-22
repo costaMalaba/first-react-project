40037148c90f86b78b25f42b3d4a0b3c097700a9
JOSHUA24-arch <mallyajoshua24@gmail.com>
2022-12-22 09:16:58 +0300

    Setting up React-Boostrap and React-Toastify

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import { Button } from 'react-bootstrap';
+
+const notify = () => {toast.success("Congratulation..!");}
 
 function App() {
   return (
     <div className="App">
       <h1>First React Project for Me</h1>
+      <Button variant="success" className='mt-3' onClick={notify}>Notify</Button>
+      <ToastContainer />
     </div>
   );
 }
